acfee031fab640fcde585c12876d07865d4f7988
oversudo <guja.markozashvili.1@btu.edu.ge>
2025-12-29 00:52:55 +0400

    Finishing shell

--- helpers/shell.go
+++ helpers/shell.go
@@ -3,22 +3,31 @@ package helpers
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func GetShellInfo() string {
-	return fmt.Sprintf("%s %s", getShell(), getShellVersion())
+	return fmt.Sprintf("%s", getShellVersion())
 }
 
-func getShell() string {
+func getShell() (string, error) {
 	shellPath := os.Getenv("SHELL")
 	lastIndex := strings.LastIndex(shellPath, "/")
 	if lastIndex != -1 {
-		return shellPath[lastIndex+1:]
+		return shellPath[lastIndex+1:], nil
 	}
-	return ""
+	return "", fmt.Errorf("shell not found")
 }
 
 func getShellVersion() string {
-	return "1"
+	shell, err := getShell()
+	if err != nil {
+		return ""
+	}
+
+	if out, err := exec.Command(shell, "--version").Output(); err == nil {
+		return strings.TrimSpace(string(out))
+	}
+	return ""
 }
